1c0f13b8feca4bd61927f86d0a1dce9cdf81b061
Luigi <posea.luigi99@gmail.com>
2023-05-17 23:49:58 +0300

    Tema 1 in curs2

--- src/curs2/Shape.java
+++ src/curs2/Shape.java
@@ -6,27 +6,30 @@ public class Shape {
 	private double radius;
 	
 	
-	public int calculateSquareArea () {
+	public int calculateSquareArea() {
 		return lenght*lenght;
 	}
 	
-	public int calculateRectangleArea () {
+	public int calculateRectangleArea() {
 		return lenght*width;
 	}
 		
-	public static void main(String[] args) {
-	}	
 	public Shape (int lenght) {
 		this.lenght=lenght;
+		System.out.println("The area of the square is: " + calculateSquareArea());
 	}
 	
 	public Shape (int lenght, int width) {
 		this.lenght = lenght;
 		this.width = width;
+		System.out.println("The area of the rectangle is: " + calculateRectangleArea());
 	}
 	
 	public Shape (double radius) {
 		this.radius = radius;
+		radius= 3.0;
+		double area = Math.PI * (radius* radius);
+		System.out.println("The area of the circle is: " + area);
+		return;
 	}
-
 }
