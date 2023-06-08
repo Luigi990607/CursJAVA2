b24f69b31b3fef377166f03a0c8f162ad9f6f217
Luigi <posea.luigi99@gmail.com>
2023-06-08 11:25:06 +0300

    curs8 final

---
+++ src/curs8/TestAgeVerifier.java
@@ -0,0 +1,22 @@
+package curs8;
+
+import java.util.Scanner;
+
+public class TestAgeVerifier {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AgeVerifier obj=new AgeVerifier();
+		System.out.println("Please enter your age: ");
+		Scanner scan=new Scanner(System.in);
+		int age=scan.nextInt();
+		try {
+		obj.checkAge(age);
+		} catch (InvalidAgeException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
