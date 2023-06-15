270039c3e85c20afbc6fd224c591816ece47c479
Luigi <posea.luigi99@gmail.com>
2023-06-15 09:09:27 +0300

    inceput tema

---
+++ src/curs7/HomeworkEx1.java
@@ -0,0 +1,22 @@
+package curs7;
+
+import java.util.Scanner;
+
+public class HomeworkEx1 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Cate elemente trebuie sa aiba array-ul? ");
+		int array= scan.nextInt();
+		array.lenght== 4;
+		}
+	
+	public static void rezolvareCuFor(int element) {
+		for (int i=0; i<numbers.length;i++) {
+			if (numbers[i] == 4) {
+				System.out.println(numbers);
+			}
+   }
+	}}
