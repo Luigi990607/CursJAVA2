0d84f8f43957621fd324d737ca2e863f67398360
Luigi <posea.luigi99@gmail.com>
2023-06-26 10:27:15 +0300

    curs13 inntermediar

--- src/curs12/TestJSONFileProcessor.java
+++ src/curs12/TestJSONFileProcessor.java
@@ -7,6 +7,17 @@ public class TestJSONFileProcessor {
 
 		JsonFileProcessor obj = new JsonFileProcessor();
 		obj.writeJsonFile();
+		obj.readJson("name");
+		obj.readJson("listaObiecte");
+		obj.readJson("email");
+		System.out.println("--------------------");
+		obj.updateJsonFile("email", "altEmail");
+		System.out.println("email after update: ");
+		obj.readJson("email");
+		
+		obj.deleteFromJson("email");
+		System.out.println("Email after delete: ");
+		obj.readJson("email");
 	}
 
 }
